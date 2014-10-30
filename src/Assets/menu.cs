c6efc5a2477ccc46deed697c864b55bdbec1f3fe
Anders Dahl <anders.b.r.dahl@gmail.com>
2014-10-30 22:18:15 +0100

    Ton of assets & Mostly functional menu scene.

---
+++ src/Assets/menu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu : MonoBehaviour {
+
+	public void startGame() {
+		Application.LoadLevel ("jaf_DEV");
+		}
+
+	public void quitGame() {
+		Application.Quit ();
+}
+
+}
