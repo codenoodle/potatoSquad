675f6c87098686dc36ea6a1f3a4f632dbd7dbc5b
Lasse G <lasse973@mail.com>
2014-10-30 22:30:11 +0100

    Adding: Player movement script Player mouse view script Placeholder "blue" Placeholder "black" Scene characterMovement_DEV

---
+++ src/Assets/playerMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		bool forward 	= Input.GetButton("Forward");
+		bool backwards 	= Input.GetButton("Backwards");
+		bool left 		= Input.GetButton("Left");
+		bool right 		= Input.GetButton("Right");
+		bool sprint 	= Input.GetButton("Sprint");
+		int MovementModifyer = 50;
+
+
+		int sprintModifyer = 1; // Default modifyer for movement
+
+		// if sprint is pressed the movement speed is altered
+		if (sprint) { sprintModifyer = 2;} 
+	
+		if (forward)
+		{
+			rigidbody.AddForce(transform.forward * MovementModifyer * sprintModifyer, ForceMode.Acceleration);
+			rigidbody.useGravity = true;
+		}
+
+		if (backwards)
+		{
+			rigidbody.AddForce(-transform.forward * MovementModifyer * sprintModifyer, ForceMode.Acceleration);
+			rigidbody.useGravity = true;
+		}
+
+		if (right)
+		{
+			rigidbody.AddForce(transform.right * MovementModifyer * sprintModifyer, ForceMode.Acceleration);
+			rigidbody.useGravity = true;
+		}
+		
+		if (left)
+		{
+			rigidbody.AddForce(-transform.right * MovementModifyer * sprintModifyer, ForceMode.Acceleration);
+			rigidbody.useGravity = true;
+		}
+
+
+	}
+}
