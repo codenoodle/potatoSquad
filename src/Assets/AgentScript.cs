33b70134b40794c44e34a77f5bfda94c6e52129a
Lasse G <lasse973@mail.com>
2014-11-02 21:52:00 +0100

    Beginning of enemy AI

---
+++ src/Assets/AgentScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentScript : MonoBehaviour {
+
+	public Transform target;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination (target.position);
+	}
+}
